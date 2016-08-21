ac14076e6219ad6613f2c04f67b60cf03716ae9c
gtp <gtp0521@gmail.com>
2016-08-21 11:51:17 +0800

    first commit

---
+++ src/com/example/intentdemo1/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.intentdemo1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+	private Button button1;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		button1 = (Button) findViewById(R.id.button_1);
+		button1.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent("com.example.intentdemo1.ACTION_START");
+				intent.addCategory("com.example.intentDemo1.MY_CATEGORY");
+				startActivity(intent);
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
